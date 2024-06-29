1205de46a9da84376975be565931c0d1b3dd605f
rafipopz98 <itsrealrafi@gmail.com>
2024-06-29 20:54:33 +0530

    final maybe

--- frontend/src/page/profile/EditProfileModal.jsx
+++ frontend/src/page/profile/EditProfileModal.jsx
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
-const EditProfileModal = () => {
+const EditProfileModal = ({ authUser }) => {
   const [formData, setFormData] = useState({
     fullName: "",
     username: "",
@@ -14,6 +16,56 @@ const EditProfileModal = () => {
   const handleInputChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
+
+  const queryClient = useQueryClient();
+
+  const {
+    mutate: editMetadata,
+    isPending: isEditingMetadata,
+    error,
+    isError,
+  } = useMutation({
+    mutationFn: async () => {
+      try {
+        const res = await fetch("/api/users/update", {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            body: JSON.stringify({ formData }),
+          },
+        });
+        const data = await res.json();
+        if (!res.ok) throw new Error(data.error || "failed to update");
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    onSuccess: () => {
+      toast.success("updated successfully");
+      Promise.all([
+        queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+        queryClient.invalidateQueries({ queryKey: ["userProfile"] }),
+      ]);
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+
+  useEffect(() => {
+    if (authUser) {
+      setFormData({
+        fullName: authUser.fullName,
+        username: authUser.username,
+        email: authUser.email,
+        bio: authUser.bio,
+        link: authUser.link,
+        newPassword: "",
+        currentPassword: "",
+      });
+    }
+  }, [authUser]);
 
   return (
     <>
@@ -32,7 +84,7 @@ const EditProfileModal = () => {
             className="flex flex-col gap-4"
             onSubmit={(e) => {
               e.preventDefault();
-              alert("Profile updated successfully");
+              editMetadata();
             }}
           >
             <div className="flex flex-wrap gap-2">
@@ -97,7 +149,7 @@ const EditProfileModal = () => {
               onChange={handleInputChange}
             />
             <button className="btn btn-primary rounded-full btn-sm text-white">
-              Update
+              {isEditingMetadata ? "Updating" : "Update"}
             </button>
           </form>
         </div>
--- frontend/src/page/profile/ProfilePage.jsx
+++ frontend/src/page/profile/ProfilePage.jsx
@@ -6,13 +6,14 @@ import ProfileHeaderSkeleton from "../../components/skeletons/ProfileHeaderSkele
 import EditProfileModal from "./EditProfileModal.jsx";
 
 import { POSTS } from "../../utils/db/dummy";
-
+import useFollow from "../../hooks/useFollow.jsx";
 import { FaArrowLeft } from "react-icons/fa6";
 import { IoCalendarOutline } from "react-icons/io5";
 import { FaLink } from "react-icons/fa";
 import { MdEdit } from "react-icons/md";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { formatMemberSinceDate } from "../../utils/date/date.js";
+import toast from "react-hot-toast";
 
 const ProfilePage = () => {
   const [coverImg, setCoverImg] = useState(null);
@@ -23,8 +24,6 @@ const ProfilePage = () => {
   const profileImgRef = useRef(null);
 
   const { username } = useParams();
-
-  const isMyProfile = true;
 
   const {
     data: user,
@@ -57,6 +56,46 @@ const ProfilePage = () => {
       reader.readAsDataURL(file);
     }
   };
+  const queryClient = useQueryClient();
+  const { follow, isPending } = useFollow();
+  const { data: authUser } = useQuery({ queryKey: ["authUser"] });
+
+  const isMyProfile = authUser._id === user?._id;
+  const amIFollowing = authUser?.following.includes(user?._id);
+
+  const {
+    mutateAsync: editProfile,
+    isPending: isEditing,
+    error,
+    isError,
+  } = useMutation({
+    mutationFn: async () => {
+      try {
+        const res = await fetch("/api/users/update", {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            body: JSON.stringify({ coverImg, profileImg }),
+          },
+        });
+        const data = await res.json();
+        if (!res.ok) throw new Error(data.error || "failed to update");
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    onSuccess: () => {
+      toast.success("updated successfully");
+      Promise.all([
+        queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+        queryClient.invalidateQueries({ queryKey: ["userProfile"] }),
+      ]);
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
 
   useEffect(() => {
     refetch();
@@ -134,21 +173,27 @@ const ProfilePage = () => {
                 </div>
               </div>
               <div className="flex justify-end px-4 mt-5">
-                {isMyProfile && <EditProfileModal />}
+                {isMyProfile && <EditProfileModal authUser={authUser} />}
                 {!isMyProfile && (
                   <button
                     className="btn btn-outline rounded-full btn-sm"
-                    onClick={() => alert("Followed successfully")}
+                    onClick={() => follow(user._id)}
                   >
-                    Follow
+                    {isPending && "Loading..."}
+                    {!isPending && amIFollowing && "Unfollow"}
+                    {!isPending && !amIFollowing && "Unfollow"}
                   </button>
                 )}
                 {(coverImg || profileImg) && (
                   <button
                     className="btn btn-primary rounded-full btn-sm text-white px-4 ml-2"
-                    onClick={() => alert("Profile updated successfully")}
+                    onClick={async () => {
+                      await editProfile();
+                      setProfileImg(null);
+                      setCoverImg(null);
+                    }}
                   >
-                    Update
+                    {isEditing ? "Updating" : "Update"}
                   </button>
                 )}
               </div>
