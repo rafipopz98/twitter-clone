3b7220aa471f911299c619a969924ec1851004da
rafipopz98 <itsrealrafi@gmail.com>
2024-06-29 11:30:03 +0530

    user model

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ import authRouter from "./routes/authRouter.js";
 import dotenv from 'dotenv'
 import connectDB from "./db/connectDB.js";
 import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
+import userRoute from './routes/userRouter.js'
+
 dotenv.config()
 const app = express();
 
@@ -10,8 +13,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 
 app.use("/auth", authRouter)
+app.use("/users", userRoute)
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`listening to port ${PORT}`)
